7a18e08ebf7e2a6182cf5484b676a3aaaef2f71c
Andrew J. Camenga <ajcam@ajcindustries.com>
2024-09-06 10:29:32 -0400

    fixing regex to allow periods in addition to colons, fixing 2Sa regex, fixing number of verses in 3JO -- KJV had only 14, modern translations split verse 14 to add 15

--- scriptures/texts/base.py
+++ scriptures/texts/base.py
@@ -23,10 +23,10 @@ class Text:
             self.scripture_re = re.compile(
                 r'\b(?P<BookTitle>%s)\s*' \
                  '(?P<ChapterNumber>\d{1,3})' \
-                 '(?:\s*:\s*(?P<VerseNumber>\d{1,3}))?' \
+                 '(?:\s*[.:]\s*(?P<VerseNumber>\d{1,3}))?' \
                  '(?:\s*[-\u2013\u2014]\s*' \
-                 '(?P<EndChapterNumber>\d{1,3}(?=\s*:\s*))?' \
-                 '(?:\s*:\s*)?' \
+                 '(?P<EndChapterNumber>\d{1,3}(?=\s*[.:]\s*))?' \
+                 '(?:\s*[:.]\s*)?' \
                  '(?P<EndVerseNumber>\d{1,3})?' \
                  ')?' % (self.book_re_string,), re.IGNORECASE | re.UNICODE)
 
